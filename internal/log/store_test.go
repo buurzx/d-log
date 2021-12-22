bd945c489a066a4d1f499810ab61e2c7756966e1
Pavel Ro <buurzx@gmail.com>
2021-12-22 15:03:09 +0300

    Introduced store

---
+++ internal/log/store_test.go
@@ -0,0 +1,121 @@
+package log
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var (
+	write = []byte("hello world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStoreAppendRead(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_append_read_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+
+	testAppend(t, s)
+	//r, _ := s.Read(0)
+	//fmt.Printf("File %v \n", r )
+	//fmt.Printf("Size %v \n", s.buf.Size())
+	//fmt.Printf("Size %v \n", s.size)
+	testRead(t, s)
+	testReadAt(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+	testRead(t, s)
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		fmt.Printf("n %v, pos: %v \n", n, pos)
+
+		r, _ := s.Read(0)
+		fmt.Printf("File %v \n", r)
+		fmt.Printf("Availabel %v \n", s.buf.Available())
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+	for i := uint64(1); i < 4; i++ {
+		read, err := s.Read(pos)
+		read1, err := s.Read(pos + 2*width)
+		fmt.Println()
+		fmt.Printf("File %v \n", write)
+		fmt.Printf("File %v \n", read)
+		fmt.Printf("File1 %v \n", read1)
+		fmt.Printf("POS %v \n", pos)
+		fmt.Printf("Size %v \n", s.buf.Size())
+		fmt.Printf("Size %v \n", s.size)
+		fmt.Println()
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
+	}
+}
+
+func testReadAt(t *testing.T, s *store) {
+	t.Helper()
+	for i, off := uint64(1), int64(0); i < 4; i++ {
+		b := make([]byte, lenWidth)
+		n, err := s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, lenWidth, n)
+		off += int64(n)
+
+		size := enc.Uint64(b)
+		b = make([]byte, size)
+		n, err = s.ReadAt(b, off)
+		require.NoError(t, err)
+		require.Equal(t, write, b)
+		require.Equal(t, int(size), n)
+		off += int64(n)
+	}
+}
+
+func TestStoreClose(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_close_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	s, err := newStore(f)
+	require.NoError(t, err)
+	_, _, err = s.Append(write)
+	require.NoError(t, err)
+	f, beforeSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	err = s.Close()
+	require.NoError(t, err)
+	_, afterSize, err := openFile(f.Name())
+	require.NoError(t, err)
+	require.True(t, afterSize > beforeSize)
+}
+
+func openFile(name string) (file *os.File, size int64, err error) {
+	f, err := os.OpenFile(
+		name,
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, 0, err
+	}
+	return f, fi.Size(), nil
+}
